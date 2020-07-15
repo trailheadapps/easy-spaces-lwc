87561e8f2b4672e982568b85a896fa7dd118baf3
adityanaag3 <atopalli@salesforce.com>
2020-07-15 10:17:56 +0530

    Tests for imageTile

---
+++ es-base-styles/main/default/lwc/imageTile/__tests__/imageTile.test.js
@@ -0,0 +1,68 @@
+import { createElement } from 'lwc';
+import ImageTile from 'c/imageTile';
+
+const SPACE_RECORD = {
+    Id: '0000000000',
+    Name: 'Sample 1',
+    Market__c: '000000',
+    Maximum_Capacity__c: 10,
+    Minimum_Capacity__c: 10,
+    Picture_URL__c: 'http://someurl.com/image.jpg',
+    Category__c: 'Cooking Classroom',
+    Type__c: 'Large Groups;Scheduled Activity;'
+};
+
+describe('c-image-gallery', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('displays correct image', () => {
+        const element = createElement('c-image-tile', {
+            is: ImageTile
+        });
+
+        element.record = SPACE_RECORD;
+        document.body.appendChild(element);
+
+        return Promise.resolve().then(() => {
+            const imgElement = element.shadowRoot.querySelector('img');
+            expect(imgElement).not.toBeNull();
+            expect(imgElement.src).toBe(SPACE_RECORD.Picture_URL__c);
+        });
+    });
+
+    it('displays correct css classes based on properties', () => {
+        const element = createElement('c-image-tile', {
+            is: ImageTile
+        });
+
+        element.record = SPACE_RECORD;
+        document.body.appendChild(element);
+
+        return Promise.resolve()
+            .then(() => {
+                const divElement = element.shadowRoot.querySelector('div');
+                expect(divElement).not.toBeNull();
+                expect(divElement.classList.contains('muted')).toBe(false);
+                expect(divElement.classList.contains('selected')).toBe(false);
+
+                element.muted = true;
+            })
+            .then(() => {
+                const divElement = element.shadowRoot.querySelector('div');
+                expect(divElement.classList.contains('muted')).toBe(true);
+                expect(divElement.classList.contains('selected')).toBe(false);
+
+                element.selected = true;
+            })
+            .then(() => {
+                const divElement = element.shadowRoot.querySelector('div');
+                expect(divElement.classList.contains('muted')).toBe(true);
+                expect(divElement.classList.contains('selected')).toBe(true);
+            });
+    });
+});
