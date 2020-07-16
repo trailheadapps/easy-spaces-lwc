4e7483225e037f81f8ff0205d84423f36a976261
msrivastav13 <mohith.shrivastava@salesforce.com>
2020-07-16 01:09:21 -0400

    resolve comments raised

--- es-space-mgmt/main/default/lwc/reservationList/__tests__/reservationList.test.js
+++ es-space-mgmt/main/default/lwc/reservationList/__tests__/reservationList.test.js
@@ -1,5 +1,6 @@
 /* eslint-disable @lwc/lwc/no-unexpected-wire-adapter-usages */
 import { createElement } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
 import getOpenReservations from '@salesforce/apex/reservationManagerController.getOpenReservations';
 import {
     registerApexTestWireAdapter,
@@ -9,7 +10,6 @@ import ReservationList from 'c/reservationList';
 import TILE_SELECTION_MC from '@salesforce/messageChannel/Tile_Selection__c';
 import FLOW_STATUS_CHANGE_MC from '@salesforce/messageChannel/Flow_Status_Change__c';
 import { subscribe, MessageContext, publish } from 'lightning/messageService';
-import { refreshApex } from '@salesforce/apex';
 
 // Realistic data with a list of spaces
 const mockOpenReservationsRecords = require('./data/getOpenReservations.json');
@@ -42,7 +42,40 @@ describe('c-reservation-list', () => {
         jest.clearAllMocks();
     });
 
-    it('getOpenReservations @wire data', () => {
+    it('displays reservation tiles when there are records from @wire getOpenReservations', () => {
+        const element = createElement('c-reservation-list', {
+            is: ReservationList
+        });
+        document.body.appendChild(element);
+
+        getOpenReservationsAdapter.emit(mockOpenReservationsRecords);
+        // Return a promise to wait for any asynchronous DOM updates. Jest
+        // will automatically wait for the Promise chain to complete before
+        // ending the test and fail the test if the promise rejects.
+        return Promise.resolve().then(() => {
+            // check if the text is rendered fine
+            const openReservationParagraphElement = element.shadowRoot.querySelector(
+                'p.slds-text-heading_small'
+            );
+            expect(openReservationParagraphElement.textContent).toBe(
+                'Open Reservations:'
+            );
+            // check if tile elements are rendered properly
+            const reservationTileElements = element.shadowRoot.querySelectorAll(
+                'c-reservation-tile'
+            );
+            expect(reservationTileElements.length).toBe(
+                mockOpenReservationsRecords.length
+            );
+            reservationTileElements.forEach((index, tile) => {
+                expect(tile.reservation).toStrictEqual(
+                    mockOpenReservationsRecords[index]
+                );
+            });
+        });
+    });
+
+    it('subscribes to flow_status_change_mc message channel', () => {
         const element = createElement('c-reservation-list', {
             is: ReservationList
         });
@@ -51,6 +84,13 @@ describe('c-reservation-list', () => {
         // Validate if pubsub got registered after connected to the DOM
         expect(subscribe).toHaveBeenCalled();
         expect(subscribe.mock.calls[0][1]).toBe(FLOW_STATUS_CHANGE_MC);
+    });
+
+    it('gets @wire reservations data', () => {
+        const element = createElement('c-reservation-list', {
+            is: ReservationList
+        });
+        document.body.appendChild(element);
 
         getOpenReservationsAdapter.emit(mockOpenReservationsRecords);
         // Return a promise to wait for any asynchronous DOM updates. Jest
@@ -64,17 +104,10 @@ describe('c-reservation-list', () => {
             expect(reservationTileElements.length).toBe(
                 mockOpenReservationsRecords.length
             );
-            // check if the text is rendered fine
-            const openReservationParagraphElement = element.shadowRoot.querySelector(
-                'p.slds-text-heading_small'
-            );
-            expect(openReservationParagraphElement.textContent).toBe(
-                'Open Reservations:'
-            );
         });
     });
 
-    it('subscribe to flow status change message', () => {
+    it('refreshes reservations when LMS message is received', () => {
         const element = createElement('c-reservation-list', {
             is: ReservationList
         });
@@ -95,19 +128,55 @@ describe('c-reservation-list', () => {
         // will automatically wait for the Promise chain to complete before
         // ending the test and fail the test if the promise rejects.
         return Promise.resolve().then(() => {
-            // Select elements for validation
-            const reservationTileElements = element.shadowRoot.querySelectorAll(
-                'c-reservation-tile'
-            );
-            expect(reservationTileElements.length).toBe(0);
             expect(subscribe).toHaveBeenCalled();
             expect(subscribe.mock.calls[0][1]).toBe(FLOW_STATUS_CHANGE_MC);
-            // test refresh method has been called as a result of LMS event
             expect(refreshApex).toHaveBeenCalled();
         });
     });
 
-    it('select a reservation tile from the list and test for Tile Selection LMS publish', () => {
+    it('dispatches reservationselect event', () => {
+        const element = createElement('c-reservation-list', {
+            is: ReservationList
+        });
+        document.body.appendChild(element);
+
+        getOpenReservationsAdapter.emit(mockOpenReservationsRecords);
+
+        // Return a promise to wait for any asynchronous DOM updates. Jest
+        // will automatically wait for the Promise chain to complete before
+        // ending the test and fail the test if the promise rejects.
+        return Promise.resolve().then(() => {
+            // Select tile element to select and dispatch event
+            const reservationTileElements = element.shadowRoot.querySelectorAll(
+                'c-reservation-tile'
+            );
+            // payload to publish
+            const PAYLOAD = {
+                reservationId: mockOpenReservationsRecords[0].Id,
+                marketId: mockOpenReservationsRecords[0].Market__c,
+                customerName: mockOpenReservationsRecords[0].Contact__r.Name
+            };
+            // dispatch reservationselect event
+            const reservationselectevt = new CustomEvent('reservationselect', {
+                detail: PAYLOAD
+            });
+            reservationTileElements[0].dispatchEvent(reservationselectevt);
+
+            expect(publish).toHaveBeenCalled();
+            const FINAL_PAYLOAD = {
+                tileType: 'reservation',
+                properties: PAYLOAD
+            };
+            // assert that the publish was called with the correct params
+            expect(publish).toHaveBeenCalledWith(
+                undefined,
+                TILE_SELECTION_MC,
+                FINAL_PAYLOAD
+            );
+        });
+    });
+
+    it('mutes reservation tiles not selected', () => {
         const element = createElement('c-reservation-list', {
             is: ReservationList
         });
@@ -131,38 +200,21 @@ describe('c-reservation-list', () => {
                 const reservationTileElements = element.shadowRoot.querySelectorAll(
                     'c-reservation-tile'
                 );
-                expect(reservationTileElements.length).toBe(
-                    mockOpenReservationsRecords.length
-                );
-
                 const reservationselectevt = new CustomEvent(
                     'reservationselect',
                     {
                         detail: PAYLOAD
                     }
                 );
+                // select first tile
                 reservationTileElements[0].dispatchEvent(reservationselectevt);
             })
             .then(() => {
-                expect(publish).toHaveBeenCalled();
-                const FINAL_PAYLOAD = {
-                    tileType: 'reservation',
-                    properties: PAYLOAD
-                };
-                // assert that the publish was called with the correct params
-                expect(publish).toHaveBeenCalledWith(
-                    undefined,
-                    TILE_SELECTION_MC,
-                    FINAL_PAYLOAD
-                );
-                // Check that all other components apart from the one selected is muted.
-                // Select elements for validation
                 const reservationTileElements = element.shadowRoot.querySelectorAll(
                     'c-reservation-tile'
                 );
-
-                let mutedElements = [];
-
+                const mutedElements = [];
+                // collect all muted elements
                 reservationTileElements.forEach((reservationTile) => {
                     if (
                         reservationTile.reservation.record.Id !==
@@ -171,10 +223,7 @@ describe('c-reservation-list', () => {
                         mutedElements.push(reservationTile);
                     }
                 });
-
-                expect(reservationTileElements.length).toBe(
-                    mockOpenReservationsRecords.length
-                );
+                // assert not selected elements are muted
                 mutedElements.forEach((reservationTile) =>
                     expect(reservationTile.reservation.muted).toBe(true)
                 );
@@ -182,6 +231,8 @@ describe('c-reservation-list', () => {
     });
 
     it('shows error panel element', () => {
+        const WIRE_ERROR = 'Custom Exception';
+
         // Create initial element
         const element = createElement('c-reservation-tile', {
             is: ReservationList
@@ -189,7 +240,7 @@ describe('c-reservation-list', () => {
         document.body.appendChild(element);
 
         // Emit error from @wire
-        getOpenReservationsAdapter.error();
+        getOpenReservationsAdapter.error(WIRE_ERROR);
 
         // Return a promise to wait for any asynchronous DOM updates. Jest
         // will automatically wait for the Promise chain to complete before
@@ -199,20 +250,21 @@ describe('c-reservation-list', () => {
                 'c-error-panel'
             );
             expect(errorPanelEl).not.toBeNull();
+            expect(errorPanelEl.errors.body).toBe(WIRE_ERROR);
             expect(errorPanelEl.friendlyMessage).toBe(
                 'There was an issue loading reservations.'
             );
         });
     });
 
-    it('renders reservation list with no tile element', () => {
+    it('renders reservation list with no tile element when @wire getOpenReservations returns 0 rows', () => {
         // Create initial element
         const element = createElement('c-reservation-tile', {
             is: ReservationList
         });
         document.body.appendChild(element);
 
-        // Emit error from @wire
+        // Emit empty response from @wire
         getOpenReservationsAdapter.emit([]);
 
         // Return a promise to wait for any asynchronous DOM updates. Jest
--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,10 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 module.exports = {
     ...jestConfig,
     // add any custom configurations here
-    // add any custom configurations here
     moduleNameMapper: {
         '^lightning/messageService$':
-            '<rootDir>/es-space-mgmt/test/jest-mocks/lightning/messageService'
+            '<rootDir>/es-space-mgmt/test/jest-mocks/lightning/messageService',
+        '^lightning/navigation$':
+            '<rootDir>/es-space-mgmt/test/jest-mocks/lightning/navigation'
     }
 };
