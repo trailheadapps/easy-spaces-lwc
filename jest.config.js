8e0c67e34509eb2b6154c76ad91b2c69ccbec0ea
Mohith Shrivastava <msrivastav13@gmail.com>
2020-07-16 07:47:00 -0400

    tests for lwc reservationHelperForm (#128)
    * tests for lwc reservationHelperForm
    
    * add some more coverage
    
    * address comments
    
    * fix test names and code readability

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
     ...jestConfig,
     // add any custom configurations here
     moduleNameMapper: {
+        '^lightning/flowSupport$':
+            '<rootDir>/es-space-mgmt/test/jest-mocks/lightning/flowSupport',
         '^lightning/navigation$':
             '<rootDir>/es-space-mgmt/test/jest-mocks/lightning/navigation',
         '^lightning/messageService$':
