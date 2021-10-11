a7ff998e52988ec67b99b36958bc503439145f38
Alba Rivas <arivas@salesforce.com>
2021-10-11 14:23:01 +0200

    Remove .eslintignore (#294)

--- es-base-styles/main/default/aura/AppPage_4_8/AppPage_4_8Controller.js
+++ es-base-styles/main/default/aura/AppPage_4_8/AppPage_4_8Controller.js
@@ -1,9 +1,11 @@
 ({
-    handleToggle: function (component, event, helper) {
+    handleToggle: function (component) {
         var collapse = component.get('v.collapsedView');
-        component.set('v.collapsedView', !collapse);
         var leftColumn = component.find('leftColumn');
         var rightColumn = component.find('rightColumn');
+
+        component.set('v.collapsedView', !collapse);
+
         $A.util.toggleClass(leftColumn, 'collapsed');
         $A.util.toggleClass(leftColumn, 'expanded');
         $A.util.toggleClass(rightColumn, 'slds-size--12-of-12');
