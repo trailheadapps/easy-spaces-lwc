0f1debb7979f2cb98a18ab89dc7c4cdd523646fe
Alba Rivas <arivas@salesforce.com>
2021-10-11 13:45:35 +0200

    Fix linting problems with aura

--- es-base-code/main/default/aura/openRecordAction/openRecordActionController.js
+++ es-base-code/main/default/aura/openRecordAction/openRecordActionController.js
@@ -1,16 +1,17 @@
 ({
-    //method name required by lightning:availableForFlowActions interface
-    invoke: function (component, event, helper) {
+    // Method name required by lightning:availableForFlowActions interface
+    invoke: function (component) {
         var recordId = component.get('v.recordId');
         var sobject = component.get('v.sobject');
         var workspaceAPI = component.find('workspace');
+
         workspaceAPI
             .openTab({
                 url: '/lightning/r/' + sobject + '/' + recordId + '/view'
             })
             .then(function (response) {
                 workspaceAPI.focusTab({ tabId: response });
-                //local action 'opens' before flow changes hit cache, so we need to refresh to grab current data
+                // Local action 'opens' before flow changes hit cache, so we need to refresh to grab current data
                 workspaceAPI.refreshTab({
                     tabId: response,
                     includeAllSubtabs: false
